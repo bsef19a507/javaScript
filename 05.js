ec855352805f729825b1c3648c944ec3a693d6ef
redmoon <bsef19a507@pucit.edu.pk>
2022-06-24 00:41:22 +0500

    string length property

--- 05.js
+++ 05.js
@@ -1,8 +1,9 @@
 // string indexing...
 
-let str = "javascript";
+let str = "javascript   ";
 
 // j a v a s c r i p t
 // 0 1 2 3 4 5 6 7 8 9
-
-console.log(str[0]);
+console.log("String is : "+ str);
+console.log("Character at 0 index : "+ str[0]);
+console.log("Length of string : "+ str.length); // spaces are counted in length
