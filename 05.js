e26a993168c0c46272153b8b4af47720833e51a5
redmoon <bsef19a507@pucit.edu.pk>
2022-06-24 00:44:44 +0500

    string indexing

--- 05.js
+++ 05.js
@@ -1,9 +1,10 @@
 // string indexing...
 
-let str = "javascript   ";
+let str = "javascript";
 
 // j a v a s c r i p t
 // 0 1 2 3 4 5 6 7 8 9
 console.log("String is : "+ str);
 console.log("Character at 0 index : "+ str[0]);
 console.log("Length of string : "+ str.length); // spaces are counted in length
+console.log("Last character : "+ str[str.length - 1]); // to print last character
