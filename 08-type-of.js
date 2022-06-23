bd2dbbbb753ec08729299733978965c235c96d74
redmoon <bsef19a507@pucit.edu.pk>
2022-06-24 01:47:24 +0500

    Number.MAX_SAFE_INTEGER

--- 08-type-of.js
+++ 08-type-of.js
@@ -10,11 +10,30 @@ let bool_08 =  true;
 // console.log(typeof name_08);
 // console.log(typeof bool_08);
 
-let num08 = "10";
-let num2_08 = "20";
+//let num08 = "10"; // +"10" converted to number or +num08..
+// ++num08; // number
+// let num2_08 = "20"; 
 
 // plus sign with string variable containig number will be number like +num08...
 
-let num3_08 = +num08 + +num2_08;
+// let num3_08 = +num08 + + num2_08;
 
-console.log(num3_08);
+//console.log(typeof num08);
+
+// let aboutMe = "My name is " + name_08 + " and my age is "+ age; =>  hard quote string
+// let aboutMe = `My name is ${name_08} and my age is ${age}`; // dollar sign plus + place holder...
+
+// console.log(aboutMe);
+
+// const myname; => Error const variable must be assigned value..
+
+// let myname ;
+//  console.log(typeof myname); // undefined type..
+//  myname = "David";
+//  console.log(typeof myname,myname); // string type..
+
+// let myname = null;
+
+// console.log(typeof myname);
+
+console.log(Number.MAX_SAFE_INTEGER);  
