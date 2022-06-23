10a2aa51a089c4a8a62384d48a288a5752f60a73
redmoon <bsef19a507@pucit.edu.pk>
2022-06-24 01:29:10 +0500

    int string conversion

--- 08-type-of.js
+++ 08-type-of.js
@@ -4,7 +4,17 @@ let name_08 = "David";
 
 let bool_08 =  true;
 
+// age = age + ""; => convert age to string
+//age = String(age);  // => convert age to string
+// console.log(typeof age);
+// console.log(typeof name_08);
+// console.log(typeof bool_08);
 
-console.log(typeof age);
-console.log(typeof name_08);
-console.log(typeof bool_08);
+let num08 = "10";
+let num2_08 = "20";
+
+// plus sign with string variable containig number will be number like +num08...
+
+let num3_08 = +num08 + +num2_08;
+
+console.log(num3_08);
